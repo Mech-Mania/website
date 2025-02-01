ac45dca347179541aba60de99b6ed4446ef28018
derekGou <derekgou19@gmail.com>
2025-02-01 18:49:27 -0500

    portrait bug fix, additional headshots

--- src/components/team/team.tsx
+++ src/components/team/team.tsx
@@ -1,8 +1,8 @@
 function Team(props:any) {
     return (
         <>
-            <div className="w-full h-full aspect-square">
-                <img className="min-w-full min-h-full" src={'/team'+props.src}></img>
+            <div className="w-full h-full flex items-center justify-center overflow-hidden">
+                <img className="w-full h-full object-cover" src={'/team'+props.src}></img>
             </div>
             <div className="opacity-0 hover:opacity-100 transition-all p-8 flex items-center justify-center bg-[#000a] absolute w-full h-full">
                 <p className="pointer-events-none text-center text-[0.8rem] text-white">{props.name}</p>
