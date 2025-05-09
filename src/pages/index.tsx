4049c0f6491849a577e1ffab2765ff56c44127f7
Derek Gou <derekgou19@gmail.com>
2025-05-09 10:46:12 -0400

    amit is lookin for luv (krs)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -257,7 +257,7 @@ function Home() {
                             <img id="loaderShadow" className="" src="mechmania.svg"></img>
                             <div className={"box-border w-fit overflow-hidden flex flex-row items-start justify-start"}>
                                 <div className="pt-8 pl-0 lg:pt-0 lg:pl-16 lg:w-auto flex flex-col items-start justify-center">
-                                    <p className="w-full text-center lg:text-left">May 12 - More Info Coming Soon</p>
+                                    <p className="w-full text-center lg:text-left">May 12 - E5/E7 @UW</p>
                                     <h1 className="w-full text-center lg:text-left">MechMania</h1>
                                     <h2 className="w-full text-center lg:text-left">Where Metal Meets Mind </h2>
                                 </div>
