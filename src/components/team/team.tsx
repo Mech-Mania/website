359acd8cb24f56c6fee6f5532f8d43d0e728b579
CamLeaf <bdaace09@gmail.com>
2025-07-02 21:01:02 -0400

    test better team making

--- src/components/team/team.tsx
+++ src/components/team/team.tsx
@@ -1,11 +1,41 @@
 import Member from './member';
 import Wheel from '../gears/wheel';
+
+const members = [
+    'Loukas Juritsch',
+    'Gavin William Lyle Heatherington',
+    'My Lan Tight',
+    'Amit Weis',
+    'Taran Flora',
+    'William Babapulle',
+    'Daniel Li',
+    'Derek Gou'
+]
+const wideDisplay = 4; // Amount of members shown in each row
+const smallDispaly = 2;
+
+
+
 function Team(props:any) {
+    
     return (
         window.innerWidth >= 1024 ? 
         <>
         <div className="gap-16 flex-col flex" style={{ maxHeight: "100%" }}>
-            <div className="items-center justify-center flex flex-row gap-16">
+            {members.map((member, index) => (
+                    // <li key={index}
+                    //     style={{
+                    //         marginBottom: '10px',
+                    //         color: 'blue',
+                    //         cursor: 'pointer'
+                    //     }}>
+                    //     Number {number}
+                    // </li>
+                    <Wheel>
+                        <Member name={member} src={'/' + member.split(' ')[0] + '.png'}/>
+                    </Wheel>
+            ))}
+            {/* <div className="items-center justify-center flex flex-row gap-16">
                 <Wheel>
                     <Member name="Loukas Juritsch" src="/loukas.png"/>
                 </Wheel>
@@ -30,10 +60,11 @@ function Team(props:any) {
                     <Member name="Daniel Li" src="/daniel.png"/>
                 </Wheel>
                 <Wheel>
-                    <Member name="Derek Gou" src="/derek.png"/>
+                    <Member name="Alexander" src="/Alexander.png"/>
                 </Wheel>
-            </div>
+            </div> */}
         </div>
+
         </>
 
         :
