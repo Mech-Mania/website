b76b62095248462ca83cff1655e4945dedbcdd47
CamLeaf <bdaace09@gmail.com>
2025-07-02 22:05:55 -0400

    Finally team/members are a component

--- src/components/team/team.tsx
+++ src/components/team/team.tsx
@@ -13,9 +13,6 @@ const members = [
 const wideDisplay = 4; // Amount of members shown in each row
 const smallDisplay = 2;
 
-//2
-//0 4 
-//
 
 function create_member(index:number, member:string){
 
@@ -32,15 +29,14 @@ function create_member(index:number, member:string){
 }
 
 function Team(props:any) {
-    
+    let per_row_count:number = (window.innerWidth >= 1024) ? wideDisplay : smallDisplay
     return (
-        window.innerWidth >= 1024 ? 
         <>
         <div className="gap-16 flex-col flex" style={{ maxHeight: "100%" }}>
-            {[...Array(Math.ceil(members.length/wideDisplay)).keys()].map((e,index)=>(
+            {[...Array(Math.ceil(members.length/per_row_count)).keys()].map((e,index)=>(
                 <div className="items-center flex flex-row gap-16">
                 
-                {members.slice(index*4,(members.length+1 > index*4+4) ? index*4+4: members.length+1).map((member, loc_index) => (
+                {members.slice(index*per_row_count,(members.length+1 > index*per_row_count+per_row_count) ? index*per_row_count+per_row_count: members.length+1).map((member, loc_index) => (
 
                         <div>
                             {create_member(index+loc_index, member)}
@@ -48,77 +44,11 @@ function Team(props:any) {
                 ))}
                 </div>
             ))}
-            
-            {/* <div className="items-center justify-center flex flex-row gap-16">
-                <Wheel>
-                    <Member name="Loukas Juritsch" src="/loukas.png"/>
-                </Wheel>
-                <Wheel dir>
-                    <Member name="Gavin William Lyle Heatherington" src="/gavin.png"/>
-                </Wheel>
-                <Wheel>
-                    <Member name="My Lan Tight" src="/my.png"/>
-                </Wheel>
-                <Wheel dir>
-                    <Member name="Amit Weis" src="/amit.png"/>
-                </Wheel>
-            </div>
-            <div className="items-center justify-center flex flex-row gap-16">
-                <Wheel dir>
-                    <Member name="Taran Flora" src="/taran.png"/>
-                </Wheel>
-                <Wheel>
-                    <Member name="William Babapulle" src="/william.png"/>
-                </Wheel>
-                <Wheel dir>
-                    <Member name="Daniel Li" src="/daniel.png"/>
-                </Wheel>
-                <Wheel>
-                    <Member name="Derek Gou" src="/derek.png"/>
-                </Wheel>
-            </div> */}
         </div>
 
         </>
 
-        :
-
-        <>
-        <div className="gap-16 flex-col flex" style={{ maxHeight: "100%" }}>
-            <div className="items-center justify-center flex flex-row gap-16">
-                <Wheel>
-                    <Member name="Loukas Juritsch" src="/loukas.png"/>
-                </Wheel>
-                <Wheel dir>
-                    <Member name="Gavin William Lyle Heatherington" src="/gavin.png"/>
-                </Wheel>
-            </div>
-            <div className="items-center justify-center flex flex-row gap-16">
-                <Wheel dir>
-                    <Member name="My Lan Tight" src="/my.png"/>
-                </Wheel>
-                <Wheel>
-                    <Member name="Amit Weis" src="/amit.png"/>
-                </Wheel>
-            </div>
-            <div className="items-center justify-center flex flex-row gap-16">
-                <Wheel>
-                    <Member name="Taran Flora" src="/taran.png"/>
-                </Wheel>
-                <Wheel dir>
-                    <Member name="William Babapulle" src="/william.png"/>
-                </Wheel>
-            </div>
-            <div className="items-center justify-center flex flex-row gap-16">
-                <Wheel dir>
-                    <Member name="Daniel Li" src="/daniel.png"/>
-                </Wheel>
-                <Wheel>
-                    <Member name="Derek Gou" src="/derek.png"/>
-                </Wheel>
-            </div>
-        </div>
-        </>
+        
     )
 }
 export default Team
