523acad9463c1ae132c012497d068bd472f68227
Camleaf <bdaace09@gmail.com>
2025-07-05 21:03:00 -0400

    try this

--- api/scoreboard.js
+++ api/scoreboard.js
@@ -42,6 +42,6 @@ export default async function handler(req, res) {
 
 
   } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: `Internal Server Error ${error}` });
   }
 }
