21df0d3a571c687315c806fac014ca496873f3f5
Camleaf <bdaace09@gmail.com>
2025-07-07 12:12:29 -0400

    asdasdas

--- api/scoreboard/pw.js
+++ api/scoreboard/pw.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
       return res.status(405).json({ error: `${req.method} not allowed` });
   }
-  console.log('log',req.body, process.env.SB_PW)
+  console.log('log',req.body, process.env.SB_PW, req)
   try {
     if (req.body==process.env.SB_PW){
       return res.status(200).json({ message: "Success"});
