9abb3fc6ee2a2f8194b0bdbf6ea9d2f1c937f8c5
Camleaf <bdaace09@gmail.com>
2025-07-16 18:01:48 -0400

    reanlskd

--- src/components/scoreboard/admin/admin.tsx
+++ src/components/scoreboard/admin/admin.tsx
@@ -47,23 +47,24 @@ function Admin(props:any) {
     return (
 
        <>
-        {(displayMode=='Locked')?
-        <Gears>
-            <div className='flex flex-col'>
-                {error}
-                <div style={{ display: displayPW }} className="flex-col lg:flex-row gap-2 rounded-full p-2 overflow-hidden bg-[#aaa]">
-                    <input onKeyDown={(e)=>{handleKeyPress(e)}} type="password" value={pw} onChange={(e)=>{setPW(e.target.value)}} placeholder="Passkey" className="text-center lg:text-left outline-none focus:ring focus:ring-black text-black p-4 rounded-t-full lg:rounded-l-full lg:rounded-r-none grow"></input>
-                    <button onClick={()=>{submit(pw)}} className="text-black p-4 rounded-b-full lg:rounded-l-none lg:rounded-r-full bg-white outline-none focus:ring focus:ring-black">
-                        Submit
-                    </button>
+       <Gears>
+            <div className="-left-[10vw] w-[120vw] flex justify-center flex-col items-center">
+                {(displayMode=='Locked')?
+                    <>
+                        {error}
+                        <div style={{ display: displayPW }} className="flex-col lg:flex-row gap-2 rounded-full p-2 overflow-hidden bg-[#aaa]">
+                            <input onKeyDown={(e)=>{handleKeyPress(e)}} type="password" value={pw} onChange={(e)=>{setPW(e.target.value)}} placeholder="Passkey" className="text-center lg:text-left outline-none focus:ring focus:ring-black text-black p-4 rounded-t-full lg:rounded-l-full lg:rounded-r-none grow"></input>
+                            <button onClick={()=>{submit(pw)}} className="text-black p-4 rounded-b-full lg:rounded-l-none lg:rounded-r-full bg-white outline-none focus:ring focus:ring-black">
+                                Submit
+                            </button>
+                        </div>
+                    </>
+                :
+                <div>
                 </div>
+                }
             </div>
         </Gears>
-        :
-        <Gears>
-            
-        </Gears>
-        }
        </>
     )
 }
