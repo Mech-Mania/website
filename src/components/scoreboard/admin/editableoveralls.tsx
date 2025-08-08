d5f2622999490e1f45ebdc759a194cdc626ea70a
Camleaf <bdaace09@gmail.com>
2025-08-08 14:04:20 -0400

    ok so that broke stuf

--- src/components/scoreboard/admin/editableoveralls.tsx
+++ src/components/scoreboard/admin/editableoveralls.tsx
@@ -69,7 +69,7 @@ function EditableOveralls({onSettingsChange, onScoreChange, teams, settings}:{on
                 <input type="text" className='text-4xl text-black' value={settings.pointsName} name={'pointsName'} onChange={handleSettingsChange}/>
             </div>
             {rankings.map((team:rankData,index)=>(
-            <div key={index} className="grid grid-cols-3 grid-flow-row items-center justify-start w-full gap-x-16 text-center">    
+            <div key={team.name} className="grid grid-cols-3 grid-flow-row items-center justify-start w-full gap-x-16 text-center">    
                 <p className="text-2xl">{team.rank}</p>
                 <p className="text-2xl">{team.name}</p>
                 <EditableInput value={team.points} boxName={team.name} commitFunc={onScoreChange}/>
