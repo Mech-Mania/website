d69c30135c295909a64e6a1bb86d7c0afb9312da
Camleaf <bdaace09@gmail.com>
2025-08-08 15:10:01 -0400

    fix up some legacy stuff

--- src/components/scoreboard/admin/editablequeue.tsx
+++ src/components/scoreboard/admin/editablequeue.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import './rankings.css'
-import { gameCont} from "./rankings.types"
+import { gameCont} from "../rankings.types"
+import EditableInput from "./editabletext"
 function Queue(props:any) {
 
     const [curGame, setCurGame] = useState((props.game == 'Global') ? props.gameContainer['Names'][0]: props.game)
@@ -22,14 +23,15 @@ function Queue(props:any) {
 
         <>
         <div className="grid grid-cols-2 grid-flow-row items-center justify-start w-full gap-x-16 text-center">
-                <h1 className="text-4xl">A1</h1>
-                <h1 className="text-4xl">A2</h1>
+                <h1 className="text-4xl">T1</h1>
+                <h1 className="text-4xl">T2</h1>
             </div>
             
             {/* Iterator */}
             <div className="grid grid-cols-2 grid-flow-row items-center justify-start w-full gap-x-16 text-center">    
                 <p className="text-2xl">{gameContainer.Data[curGame].A1}</p>
                 <p className="text-2xl">{gameContainer.Data[curGame].A2}</p> 
+
             </div>
         </>
     )
