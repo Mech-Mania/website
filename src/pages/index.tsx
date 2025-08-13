cca95a6514b5efdc5af82a4135a653df0ef02489
Camleaf <bdaace09@gmail.com>
2025-08-12 21:11:18 -0400

    change some dates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -131,7 +131,7 @@ function Home() {
                                 <br/>
                                 {error}
                                 <div className="mt-2 flex flex-col lg:flex-row gap-2 rounded-full p-2 overflow-hidden bg-[#aaa]">
-                                    <input onKeyDown = {(e)=>{handleKeyPress(e)}} value={email} onChange = {(e)=>{setEmail(e.target.value)}} placeholder="Join our emailing list" className="text-center lg:text-left outline-none focus:ring focus:ring-black text-black p-4 rounded-t-full lg:rounded-l-full lg:rounded-r-none grow"></input>
+                                    <input onKeyDown = {(e)=>{handleKeyPress(e)}} value={email} onChange = {(e)=>{setEmail(e.target.value)}} placeholder="Enter your email to register!" className="text-center lg:text-left outline-none focus:ring focus:ring-black text-black p-4 rounded-t-full lg:rounded-l-full lg:rounded-r-none grow"></input>
                                     <button onClick = {()=>{submit(email)}} className="text-black p-4 rounded-b-full lg:rounded-l-none lg:rounded-r-full bg-white outline-none focus:ring focus:ring-black">
                                         Submit
                                     </button>
