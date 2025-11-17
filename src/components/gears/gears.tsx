93bce17cb449213be8ac89d6bcad9abb02da1c1e
Camleaf <bdaace09@gmail.com>
2025-11-17 18:19:55 -0500

    why

--- src/components/gears/gears.tsx
+++ src/components/gears/gears.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState, useRef } from "react";
 import Tooth from "./tooth";
 import Reload from "../load/reload/reload";
-import path from "path";
 
 function Gears(props: any) {
-    // Ok so just a note to any future people working on this after me, I just inherited this code from the previous guy, and touching just about anything breaks the entire website, so leave it alone it should work fine.
+    // I've tried for so long to understand what the hell is happening here. I made some progress on reverse engineering it but honestly it's probably a waste of time.
+    // I've put some line comments around to hopefully help anyone else who tries to refactor this because before there were none
     const [color, setColor] = useState("#4440");
     const [teeth, setTeeth]: any[] = useState([]);
     const [set, setSet] = useState(0);
@@ -32,11 +32,7 @@ function Gears(props: any) {
 
     const addTeeth = () => {
         const lst = [];
-        const cornerRad = 4; // rem
-        const pathLength = 2*(contHeight+contWidth)
-        const teethCount = pathLength / 8; //4rem gap
-
-        for (let i = 0; i < teethCount; i++) {
+        for (let i = 0; i < (contWidth + contHeight) / 4 + 2; i++) {
             lst.push(
                 <div
                     className="absolute w-16 h-16 -z-10 pointer-events-none"
@@ -57,105 +53,46 @@ function Gears(props: any) {
         }
     }, [updated]);
 
-
-    const normalize = () =>{
-        return -new Date().getMilliseconds();
-    }
+    const dir = props.dir ? true : false;
 
     useEffect(() => {
-        
-        const dir = props.dir ? true : false;
-        const cornerRad = 4; // rem
-        // ///Pathlength calc: 
-        const cornerLength = (Math.PI*cornerRad) / 2 // arc of individual corner
-
-        const pathLength =  2*Math.PI*cornerRad +  //Perimeter of all 4 corners = 2*pi*cornerRadius
-                            2*(contHeight-2*cornerRad) + // We take the width in REM, then remove the width of the corners because they covered above
-                            2*(contWidth-2*cornerRad); // same as above but for height
-
-        const teethCount = Math.ceil(((contHeight+contWidth) / 4));
-
-        // At this point just find a way to make it so that pathlength/teethcount is 8
-        const delay = 1000
-        const duration = pathLength/8 * delay
-        
-        // console.log(duration,pathLength,duration/pathLength)
-        // console.log(pathLength, teethCount, pathLength/teethCount)
-
-        // Now to get how much percent each element of the animation takes up the the entire animation time so that I can calculate offsets required to make this happen
-        const cornerPercent = cornerLength/pathLength;
-        const widthPercent = (contWidth-2*cornerRad)/pathLength;
-        const heightPercent = (contHeight-2*cornerRad)/pathLength;
-        
-        // calculates the percentage through as a digit between 0 and 1 of each section
-        const order = [widthPercent,cornerPercent,heightPercent,cornerPercent,widthPercent,cornerPercent,heightPercent]
-        const offsetList:number[] = [
-            0, // at start
-        ];
-        for (let i = 1;i<8;i++){
-            offsetList.push(
-                offsetList[i-1]+order[i-1]
-            )
-        }
-
-        offsetList.push(1)
-
-        
-
-        // const pathLength = 2*(contHeight+contWidth);
-        // const teethCount = pathLength / 8;
-        // const delay = 1000;
-        // const duration = teethCount*delay;
-
-        
-
-
-        // I need to dig into this animate function because how everything works isn't right
-        // The things have different speeds on different sides
+        const toothCount = ((contWidth + contHeight) / 4 + 2);
         teethRefs.current.forEach((ref: any, index: any) => {
             if (ref) {
                 ref.getAnimations().forEach((animation: Animation) => animation.cancel()); // Clear existing animations
-                let anim:any = ref.animate(
-                    [
-                        // Top left point
-                        { transformOrigin: `${4}rem ${-contHeight + 4}rem`, transform: `rotate(0deg) translateX(${2}rem) translateY(${-contHeight - 4}rem)`,offset:0}, //loops from end
-                        
-                        
-                        // Top right point
-                        {transformOrigin: `${contWidth - 4}rem ${-contHeight + 4}rem`, transform: `rotate(0deg) translateX(${contWidth-6}rem) translateY(${-contHeight-4}rem)`,offset: offsetList[1]},
-                        // Right top point
-                        {transformOrigin: `${contWidth - 4}rem ${-contHeight + 4}rem`,transform: `rotate(90deg) translateX(${contWidth-6}rem) translateY(${-contHeight-4}rem)`, offset: offsetList[2]},
 
-                        
-                        //Right bottom point
-                        {transformOrigin: `${contWidth - 4}rem ${-4}rem`, transform: `rotate(90deg) translateX(${contWidth-6}rem) translateY(${-12}rem)`, offset: offsetList[3]},
-                        // Bottom right point
-                        { transformOrigin: `${contWidth - 4}rem ${-4}rem`, transform: `rotate(180deg) translateX(${contWidth - 6}rem) translateY(${-12}rem)`, offset: offsetList[4]},
-
-
-                        // Bottom left point
-                        { transformOrigin: `${4}rem ${-4}rem`, transform: `rotate(180deg) translateX(${2}rem) translateY(${-12}rem)`, offset: offsetList[5]},
-                        // left bottom point
-                        { transformOrigin: `${4}rem ${-4}rem`, transform: `rotate(270deg) translateX(${2}rem) translateY(${-12}rem)`, offset: offsetList[6]},
-                        
-
-                        // left top point
-                        { transformOrigin: `${4}rem ${-contHeight + 4}rem`, transform: `rotate(270deg) translateX(${2}rem) translateY(${-contHeight - 4}rem)`, offset: offsetList[7]},
-                        //top left point
-                        { transformOrigin: `${4}rem ${-contHeight + 4}rem`, transform: `rotate(360deg) translateX(${2}rem) translateY(${-contHeight - 4}rem)`, offset:1}, // this last one loops to start
-
+                // For the ref.animate below, each of the groups represents 1 point, and it's translations from the base location.
+                // The ref will animate from the position of group A to B, then from B to C, then finally from C to A. etc.
+                // transformOrigin is for rotations. It just transforms where the tooth rotates around
+                // transform is pretty self-explanatory
+                // offset represents the % complete that the animation is once it reaches this point
+                ref.animate(
+                    [   
+                        { transformOrigin: `${4}rem ${-contHeight + 4}rem`, transform: `rotate(0deg) translateX(${2}rem) translateY(${-contHeight - 4}rem)`, offset: 0 },
+                        { transformOrigin: `${contWidth - 4}rem ${-contHeight + 4}rem`, transform: `rotate(0deg) translateX(${contWidth - 6}rem) translateY(${-contHeight - 4}rem)`, offset: contWidth / (2 * contWidth + 2 * contHeight + 40) },
+                        { transformOrigin: `${contWidth - 4}rem ${-contHeight + 4}rem`, transform: `rotate(90deg) translateX(${contWidth - 6}rem) translateY(${-contHeight - 4}rem)`, offset: (contWidth + 10) / (2 * contWidth + 2 * contHeight + 40) },
+                        { transformOrigin: `${contWidth - 4}rem ${-4}rem`, transform: `rotate(90deg) translateX(${contWidth - 6}rem) translateY(${-12}rem)`, offset: (contWidth + contHeight + 10) / (2 * contWidth + 2 * contHeight + 40) },
+                        { transformOrigin: `${contWidth - 4}rem ${-4}rem`, transform: `rotate(180deg) translateX(${contWidth - 6}rem) translateY(${-12}rem)`, offset: 0.5 },
+                        { transformOrigin: `${4}rem ${-4}rem`, transform: `rotate(180deg) translateX(${2}rem) translateY(${-12}rem)`, offset: (2 * contWidth + contHeight + 20) / (2 * contWidth + 2 * contHeight + 40) },
+                        { transformOrigin: `${4}rem ${-4}rem`, transform: `rotate(270deg) translateX(${2}rem) translateY(${-12}rem)`, offset: (2 * contWidth + contHeight + 30) / (2 * contWidth + 2 * contHeight + 40) },
+                        { transformOrigin: `${4}rem ${-contHeight + 4}rem`, transform: `rotate(270deg) translateX(${2}rem) translateY(${-contHeight - 4}rem)`, offset: (2 * contWidth + 2 * contHeight + 30) / (2 * contWidth + 2 * contHeight + 40) },
+                        { transformOrigin: `${4}rem ${-contHeight + 4}rem`, transform: `rotate(360deg) translateX(${2}rem) translateY(${-contHeight - 4}rem)`, offset: 1 },
                     ],
                     {
-                        duration: duration,
+                        duration: toothCount * 1000,
                         iterations: Infinity,
-                        delay: -delay*index,
+                        delay: index * -1000 + // since each tooth should have a 1s gap between it and the next, we do this
+                                (dir ? 500 : 0) +  // to make it so that opposite dir things can mesh with current
+                                toothCount * -2000 + // assuming this one counterbalances the line below
+                                (dir ? -1 : 1) * toothCount * 1000  * (1.5 * contWidth + 2 * contHeight + 40) / (2 * contWidth + 2 * contHeight + 40), //magic bullshit
+
                         direction: dir ? "reverse" : "normal",
                     }
                 );
             }
         });
     }, [teeth, contWidth, contHeight, set]);
-    
+
     var resize = 0
     var size = -
     useEffect(() => {
--- src/components/gears/wheel.tsx
+++ src/components/gears/wheel.tsx
@@ -25,11 +25,8 @@ function Wheel(props: any) {
     };
 
     const addTeeth = () => {
-        const diameter = 8; // rem
-        const pathLength =  diameter*Math.PI; // in rem
-        const teethCount = pathLength / 4
         const lst = [];
-        for (let i = 0; i < Math.floor(teethCount); i++) {
+        for (let i = 0; i < 6; i++) {
             lst.push(
                 <div
                     className="absolute w-16 h-16 -z-10 pointer-events-none"
@@ -44,8 +41,6 @@ function Wheel(props: any) {
         setSet((prev) => prev + 1);
     };
 
-
-
     useEffect(() => {
         if (realWidth !== 0 && realHeight !== 0) {
             addTeeth();
@@ -55,17 +50,6 @@ function Wheel(props: any) {
     const dir = props.dir ? true : false;
 
     useEffect(() => {
-
-
-        const dir = props.dir ? true : false;
-        const diameter = 8; // rem
-        const pathLength =  diameter*Math.PI; // in rem
-        const teethCount = pathLength / 4;
-        const duration =  teethCount * 1000;
-        const delayRatio = teethCount/ (Math.floor(teethCount));
-
-        const delay = 1000*delayRatio;
-
         teethRefs.current.forEach((ref: any, index: any) => {
             if (ref) {
                 ref.getAnimations().forEach((animation: Animation) => animation.cancel()); // Clear existing animations
@@ -75,9 +59,9 @@ function Wheel(props: any) {
                         { transformOrigin: `${4}rem ${-realHeight + 4}rem`, transform: `rotate(360deg) translateX(${2}rem) translateY(${-realHeight - 4}rem)`, offset: 1 },
                     ],
                     {
-                        duration: duration,
+                        duration: 6 * 1000,
                         iterations: Infinity,
-                        delay: -delay*index - (dir ? 500:0),
+                        delay: (dir ? -500 : 0 ) + (index * -1000),
                         direction: dir ? "reverse" : "normal",
                     }
                 );
