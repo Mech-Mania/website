483c74320a0bfca70c633a4b5803155c8b6389f4
Camleaf <bdaace09@gmail.com>
2025-12-28 22:15:27 -0500

    Finish basic admin dash functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import {
     Route,
 } from "react-router-dom";
 import Layout from "./pages/layout";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import EmailLanding from "./pages/emailLanding.tsx";
+import Dashboard from "./pages/dashboard.tsx";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="/emailLanding" element={<EmailLanding/>}/>
           <Route path="/*" element={<Layout/>}/>
         </Routes>
