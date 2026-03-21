02049f4189276c360e2f31eabc7447ee4039b113
Camleaf <bdaace09@gmail.com>
2026-03-20 21:26:34 -0400

    got a base working

--- src/pages/scoreboardAdmin.tsx
+++ src/pages/scoreboardAdmin.tsx
@@ -2,17 +2,21 @@ import { useState, useEffect, useRef } from "react";
 import Gears from "../components/gears/gears";
 import { RankData, RankingsCont, ScoreboardScores, ScoreboardSettings } from "../components/scoreboard/scoreboard.types";
 import { createRankings } from "../components/scoreboard/scoreboard.util";
+import FieldSM from "../components/scoreboard/fieldsm";
+import PassField from "../components/layout/passwordField";
 
 function ScoreboardAdmin(props:{width:number}) {
 
     const [names,setNames] = useState<string[]>([]);
-    const [games,setGames] = useState<ScoreboardScores>({});
+    const [scores,setScores] = useState<ScoreboardScores>({});
     const [current, setCurrent] = useState<string>("overall");
     const [rankings, setRankings] = useState<RankingsCont>({overall:[]});
     const [enabled, setEnabled] = useState<boolean>(false);
     const [settings, setSettings] = useState<ScoreboardSettings>({overall:{pointsName:"Points",descending:true}});
     
     const [locked, setLocked] = useState<boolean>(true);
+    const [pw, setPW] = useState<string>('');
+    const [RRHandle,refreshRankings] = useState<number>(0);
 
     const requestFromAPI = async () => {
         
@@ -58,7 +62,7 @@ function ScoreboardAdmin(props:{width:number}) {
         if (responseGameScores.ok){
             const body = await responseGameScores.json();
             setRankings(createRankings(body.content,namebody.content,statusBody.settings));
-            setGames(body.content);
+            setScores(body.content);
         } else console.log(responseGameScores)
     }; 
 
@@ -66,24 +70,93 @@ function ScoreboardAdmin(props:{width:number}) {
         setCurrent(mode);
     };
 
+    const unlock = (name:string, value:string) => {
+        setLocked(false);
+        setPW(value); // set the PW to what was given as it is correct
+    }
+
+    const updateScore = (name:string, value:string) => {
+        setScores(prevState => ({
+            ...prevState,
+            [name]: {
+                ...prevState[name],
+                [current]: parseInt(value)
+            }
+        }));    
+
+        refreshRankings(prev=>((prev == 0)?1:0));
+    }
+
+    const onSave = async () => {
+        let response = await fetch(
+            `${__SiteBase__}/scoreboard/game/score`,
+            {
+                method : 'POST',
+                body : JSON.stringify({
+                    data:scores,
+                    password:pw
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        if (!response.ok) console.log(response);
+
+        response = await fetch(
+            `${__SiteBase__}/scoreboard/status`,
+            {
+                method : 'POST',
+                body : JSON.stringify({
+                    data: enabled,
+                    password:pw
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        if (!response.ok) console.log(response);
+
+    }
+
+
+    useEffect(()=>{
+        setRankings(createRankings(scores,names,settings))
+    },[RRHandle])
 
 
     useEffect(()=>{
         requestFromAPI()
-        
-        setInterval(requestFromAPI,40000);
     },[]);
-    
-   
 
 
     /*
      * Needs to implement rankings data first and foremost.
      */
     return (
-        enabled ? 
+        locked ?
         <>
-            <Gears key='1'>
+            <Gears key='0'>
+                <div className="-left-[10vw] w-[120vw] flex justify-center items-center">
+                    <PassField value={''} successFunc={unlock} boxName={"passfield"}/>
+                </div>    
+            </Gears>
+
+        </>
+        :
+        <>
+            <Gears key='0'>
+                <div className="cont gap-8 z-50 bg-black box-content rounded-[4rem] flex flex-col text-center w-[20vw]">
+                    <div onClick={()=>{onSave()}} className="hover:brightness-110 transition-all rounded-sm w-full pentagon-left p-4 cursor-pointer bg-white">
+                        <h2 style={{ color: 'black'}} className="transition-all text-right">Save</h2>
+                    </div>
+                    <h2>Enable public scoreboard:</h2>
+                    <input type='checkbox' checked={enabled} onChange={()=>{setEnabled(enabled ? false:true)}}/>
+                </div>
+            </Gears>
+
+            <Gears dir key='1'>
                     <div className="cont gap-8 z-50 bg-black box-content rounded-[4rem] flex flex-col text-center items-center -left-[10vw] w-[120vw]">
                         <div className='flex max-w-[96vw] gap-8'>
 
@@ -106,24 +179,16 @@ function ScoreboardAdmin(props:{width:number}) {
 
                         {rankings[current].map((team:RankData,index)=>(
                             <div key={team.name} className={`grid grid-cols-3 grid-flow-row items-center justify-start ${(window.innerWidth>800)?"w-fit min-w-[50vw]":"w-[90vw]"} gap-x-16 text-center`}>    
-                                <p className="text-2xl">{team.rank}</p>
+                                <p  className="text-2xl">{team.rank}</p>
                                 <p className="text-2xl">{team.name}</p>
-                                <p className="text-2xl">{team.points}</p>
+                                <FieldSM value={team.points} commitFunc={updateScore} boxName={team.name}/>
                             </div>
                         ))}
 
                     </div>
             </Gears>
         </>
-        :
-        <>
-            <Gears key='0'>
-                <p className="-left-[10vw] w-[120vw] flex justify-center items-center">
-                    The scoreboard is not available at this time
-                </p>
-            </Gears>
-        </>
     );
-    }
+}
 
-    export default ScoreboardAdmin;
+export default ScoreboardAdmin;
