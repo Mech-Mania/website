b40852b872ac76576dc9e2f525562332b64cbac9
Camleaf <bdaace09@gmail.com>
2026-04-22 19:59:14 -0400

    Add info document link (#13)
    * add info doc
    
    * quick fixes

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,4 @@
+import { discord_link, email_link, github_link, info_doc_link, insta_link, youtube_link } from "./hrefconstants"
 import Navbar from "./navbar"
 function Footer(props:{scrollRef:any,marginTop:number}) {
 
@@ -29,34 +30,40 @@ function Footer(props:{scrollRef:any,marginTop:number}) {
                     <div className="h-[0.125rem] w-[100%] bg-gradient-to-r from-[#444] to-[#444]"/>
 
 
-                    <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzCbZtZTTMCLGpkFmvFpBNVkdZZJNmbSfshSkxKGfrBNSFnLhTHWkMslthLwvSzctQJFwJF" target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
+                    <a href={email_link} target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
                         <img src="/icons/emailLogo.svg" className="w-6 h-6 group-hover:scale-110 ease-out duration-200"/>
                         <p>organizers@mechmania.ca</p>
                     </a>
                     
                     {/* insta */}
-                    <a href="https://www.instagram.com/mechmaniakw/" target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
+                    <a href={insta_link} target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
                         <img src="/icons/instagramLogo.svg" className="w-6 h-6 group-hover:scale-110 ease-out duration-200"/>
                         <p>mechmaniakw</p>
                     </a>
 
                     {/* github */}
-                    <a href="https://github.com/Mech-Mania" target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
+                    <a href={github_link} target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
                         <img src="/icons/githubLogo.svg" className="w-6 h-6 group-hover:scale-110 ease-out duration-200"/>
                         <p>mech-mania</p>
                     </a>
 
                     {/* youtube */}
-                    <a href="https://www.youtube.com/@MechMania-KW" target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
+                    <a href={youtube_link} target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
                         <img src="/icons/youtubeLogo.svg" className="w-6 h-6 group-hover:scale-110 ease-out duration-200"/>
                         <p>mechmania-kw</p> 
                     </a>
                     
                     {/* discord */}
-                    <a href="https://discord.gg/z5pcMevHpK" target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
+                    <a href={discord_link} target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
                         <img src="/icons/discordLogo.svg" className="w-6 h-6 group-hover:scale-110 ease-out duration-200"/>
                         <p>our discord server!</p> 
                     </a>
+
+                    <a href={info_doc_link} target="_blank" className="flex flex-row gap-2 align-middle ease-out hover:brightness-[25%] duration-200 cursor-pointer group">
+                        <img src="/icons/docsLogo.svg" className="w-6 h-6 group-hover:scale-110 ease-out duration-200"/>
+                        <p>2026 info document</p> 
+                    </a>
+
                 </div>
                 
             </div>
---
+++ src/components/layout/hrefconstants.ts
@@ -0,0 +1,9 @@
+
+export const info_doc_link:string = "https://docs.google.com/document/d/1vScmRL2N0GUlOTBg0-D0y-EhGA-HX2K0NHNl2WDQ5WY";
+
+export const insta_link:string = "https://www.instagram.com/mechmaniakw/";
+export const email_link:string = "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzCbZtZTTMCLGpkFmvFpBNVkdZZJNmbSfshSkxKGfrBNSFnLhTHWkMslthLwvSzctQJFwJF";
+export const github_link:string = "https://github.com/Mech-Mania";
+export const youtube_link:string = "https://www.youtube.com/@MechMania-KW";
+export const discord_link:string = "https://discord.gg/z5pcMevHpK";
+
