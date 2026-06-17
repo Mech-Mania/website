7662c5cae04269e5d3a649c11f7fab6027e828be
edzhang10 <edzhang10@gmail.com>
2026-06-17 16:15:21 -0400

    fix email href + small format

--- src/components/layout/hrefconstants.ts
+++ src/components/layout/hrefconstants.ts
@@ -1,8 +1,7 @@
 
 export const info_doc_link:string = "https://docs.google.com/document/d/1vScmRL2N0GUlOTBg0-D0y-EhGA-HX2K0NHNl2WDQ5WY";
-
 export const insta_link:string = "https://www.instagram.com/mechmaniakw/";
-export const email_link:string = "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzCbZtZTTMCLGpkFmvFpBNVkdZZJNmbSfshSkxKGfrBNSFnLhTHWkMslthLwvSzctQJFwJF";
+export const email_link:string = "https://mail.google.com/mail/?fs=1&to=organizers@mechmania.ca&body=I%20love%20Mechmania!&tf=cm";
 export const github_link:string = "https://github.com/Mech-Mania";
 export const youtube_link:string = "https://www.youtube.com/@MechMania-KW";
 export const discord_link:string = "https://discord.gg/z5pcMevHpK";
